4c500cda0605df6a18dee2a6d164af05e9f243f8
FlavianOMEGA <flavianmelquiades@gmail.com>
2020-07-23 23:15:06 -0300

    Placa Arrumada

--- libs/learnboard/device.d.ts
+++ libs/learnboard/device.d.ts
@@ -1,14 +1,14 @@
 declare namespace pins {
     //% fixedInstance shim=pxt::getPin(PIN_LED)
-    const LED: DigitalInOutPin;
+    const LED: PwmOnlyPin;
     //% fixedInstance shim=pxt::getPin(PIN_LED2)
-    const LED2: DigitalInOutPin;
+    const LED2: PwmOnlyPin;
     //% fixedInstance shim=pxt::getPin(PIN_LED3)
-    const LED3: DigitalInOutPin;
+    const LED3: PwmOnlyPin;
     //% fixedInstance shim=pxt::getPin(PIN_LED4)
-    const LED4: DigitalInOutPin;
+    const LED4: PwmOnlyPin;
     //% fixedInstance shim=pxt::getPin(PIN_LED5)
-    const LED5: DigitalInOutPin;
+    const LED5: PwmOnlyPin;
 
 
     //% fixedInstance shim=pxt::getPin(PIN_A4)
@@ -17,12 +17,6 @@ declare namespace pins {
     const A5: AnalogInPin;
 
 
-    //% fixedInstance shim=pxt::getPin(PIN_SCL)
-    const SCL: DigitalInOutPin;
-    //% fixedInstance shim=pxt::getPin(PIN_SDA)
-    const SDA: DigitalInOutPin;
-
-    // TODO only checked the following two for Metro M0
     //% fixedInstance shim=pxt::getPin(PIN_NEOPIXEL)
     const NEOPIXEL: DigitalInOutPin;
 }
@@ -40,7 +34,7 @@ declare namespace input {
      * Button 1
      */
     //% indexedInstanceNS=input indexedInstanceShim=pxt::getButton
-    //% block="button 1" weight=95 fixedInstance
+    //% block="button 1" weight=94 fixedInstance
     //% shim=pxt::getButton(1)
     const button1: Button;
 }
